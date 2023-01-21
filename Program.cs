1fe5d4945c58107556b5045b0b2ce89d3f167362
Ishan <ishanwijes@gmail.com>
2023-01-21 16:35:34 +1100

    added number and string variables

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
 // first, in the terminal you need to create a C# app, there are so many different types of apps for C#, you can create a web app, a software app that can be run by windows, linux or Mac etc etc. Simplest C# app to create is a console, to do this use the command: dotnet new console
 // then to run the code, use the command: dotnet run (this will automatically run Program.cs)
 
+// PascalCase is used for class names and method names, but camelCase is used for variable names
+
 
 
 using System;
@@ -14,7 +16,26 @@ public class HelloWorld {
   
   // here we create a method inside the class 
   public static void Main(string[] args) {
+    // Console is itself a built-in class and WriteLine is a built-in method for that class
     Console.WriteLine("Hello, World!");
+
+
+
+    // define a string variable
+    string userName = "Ishan";
+
+    Console.WriteLine(userName);
+
+    // can use string concatination like JavaScript
+    Console.WriteLine("Hello there " + userName);
+
+
+
+    // define a whole number
+    int hugeNumber = 43562;
+    // define a decimal number
+    // add f at end for floating value
+    float price = 3.99f;
   }
 }
 
