8b8c99a8a907a8a655cdaa1a97b2e1487abf63b7
Ishan <ishanwijes@gmail.com>
2023-01-21 16:13:01 +1100

    added console app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// C# is an object-oriented programming language
+
+// first, in the terminal you need to create a C# app, there are so many different types of apps for C#, you can create a web app, a software app that can be run by windows, linux or Mac etc etc. Simplest C# app to create is a console, to do this use the command: dotnet new console
+// then to run the code, use the command: dotnet run (this will automatically run Program.cs)
+
+
+
+using System;
+
+// here we create a class called HelloWorld
+public class HelloWorld {
+  
+  // here we create a method inside the class 
+  public static void Main(string[] args) {
+    Console.WriteLine("Hello, World!");
+  }
+}
+
