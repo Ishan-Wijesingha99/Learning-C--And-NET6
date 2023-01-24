e9579874db3332c739f18ceb2c684a0030a03ebb
Ishan <ishanwijes@gmail.com>
2023-01-24 23:08:05 +1100

    operators completed

--- 4 - operators/Program.cs
+++ 4 - operators/Program.cs
@@ -3,47 +3,65 @@ using System;
 
 
 
-namespace TryAndCatchError {
+namespace Operators {
 
-  class TryAndCatchClass {
+  class OperatorClass {
 
     static void Main(string[] args) {
-      // let's add two numbers that the user types into the terminal
-      Console.WriteLine("Please enter first number");
-      string num1String = Console.ReadLine();
-      Console.WriteLine("Please enter second number");
-      string num2String = Console.ReadLine();
-
-      // we handle possible errors by using a try-catch block
-      try
-      {
-        // here we will try and parse num1String and num2String
-        int num1 = int.Parse(num1String);
-        int num2 = int.Parse(num2String);
-
-        // log result to the console
-        Console.WriteLine(num1 + num2); 
-
-      } catch (FormatException) {
-
-        Console.WriteLine("Format exception, make sure you type an integer");
+  
+      // unary operators (switches it)
+      int num1 = 5;
+      System.Console.WriteLine(-num1); // will output -5
       
-      } catch (OverflowException) {
-      
-        Console.WriteLine("Overflow exception, make sure the number you type is not too long or too short");
-      
-      } catch (Exception) {
-      
-        // Exception accounts for all exceptions (errors), but if you only want to catch a particular type of error, you can use FormatException or OverflowException etc
-        // you can create multiple catch blocks for each different type of exception, but if you do this, the Exception catch block must come last
+      bool isSunny = true;
+      System.Console.WriteLine(!isSunny); // will output false
 
-        Console.WriteLine("General exception");
 
-        // if you just write throw, it will come up with the specific error in the terminal
-        // throw 
-      } finally {
-        // whatever is in the finally codeblock will be executed when the try and catch are done, regardless of where the execution stopped
-      }
+
+      // increment and decrement operators
+      int num2 = 0;
+
+      // increase by 1
+      num1++; // only takes effect on next line
+      ++num1; // takes effect on this line
+
+      // decrease by 1
+      num1--; // only takes effect on next line
+      --num1; // takes effect on this line
+
+
+
+      // remainder operator, it will return the remainder of the division
+      int remainder = 11 % 2;
+      System.Console.WriteLine(remainder);
+
+
+
+      // relational and type operators
+      bool isLower = 10 < 3; // false
+      bool isHigher = 7 > 4; // true
+      bool isHigherOrEqual = 6 >= 6; // true
+      bool isLowerOrEqual = 4 <= 1; // true
+
+
+
+      // equality operator
+      int num3 = 10;
+      int num4 = 13;
+
+      // there is no == vs === like in javascript, there is only strict equality
+      // can't compare two values of different types
+      System.Console.WriteLine(num3 == num4); // false
+
+      // there is also only one unequal operator
+      System.Console.WriteLine(num3 != num4); // true
+
+      
+
+      // conditional operators
+      bool isLowerAndSunny = isLower && isSunny; // false && true ends up with false
+
+      bool isLowerOrSunny = isLower || isSunny; // false || true ends up with true 
     }
 
   }
