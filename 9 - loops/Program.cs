06a47c4fddd4e180a38f85811a22f045aaa62372
Ishan <ishanwijes@gmail.com>
2023-01-25 23:35:46 +1100

    for-loop completed

--- 9 - loops/Program.cs
+++ 9 - loops/Program.cs
@@ -9,6 +9,23 @@ namespace Loops {
 
     static void Main(string[] args) {
       
+      // for-loop
+      // literally the same as in javascript
+      for(int i=0; i < 10; i++) {
+        System.Console.WriteLine("i is equal to {0}", i);
+      }
+      System.Console.WriteLine("First for-loop completed");
+
+      for(int i=0; i <= 50; i+=7) {
+        System.Console.WriteLine("i is equal to {0}", i);
+      }
+      System.Console.WriteLine("Second for-loop completed");
+
+
+
+      // while-loop
+      // literally the same as javascript
+
     }
 
   }
