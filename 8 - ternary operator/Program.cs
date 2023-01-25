26bc990e74d89d575c3da3e2e0cd841c7fab36ab
Ishan <ishanwijes@gmail.com>
2023-01-25 23:23:35 +1100

    completed ternary operator

---
+++ 8 - ternary operator/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+
+
+namespace TernaryOperator {
+
+  class TernaryOperatorClass {
+
+    static void Main(string[] args) {
+      
+      // ternary operators works exactly the same as in javascript
+      bool ateFood = true;
+
+      // ternary operators can only be used to store something as a variable
+      string printedString = ateFood ? "He ate food!" : "He did not eat food";
+
+      System.Console.WriteLine(printedString);
+
+      // you can chain ternary operators in C#
+      bool drankWater = false;
+      string finalString = drankWater ? "He drank water" : (ateFood ? "He ate food but did not drink water" : "He did not eat food or drink water");
+
+      System.Console.WriteLine(finalString);
+    }
+
+  }
+
+}
