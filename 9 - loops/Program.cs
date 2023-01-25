fece6946d82c4cbfede81fad16c2daf72eaad278
Ishan <ishanwijes@gmail.com>
2023-01-26 00:26:07 +1100

    do-while loops completed

--- 9 - loops/Program.cs
+++ 9 - loops/Program.cs
@@ -11,6 +11,7 @@ namespace Loops {
       
       // for-loop
       // literally the same as in javascript
+      // the value of i is incremented at the end of each iteration
       for(int i=0; i < 10; i++) {
         System.Console.WriteLine("i is equal to {0}", i);
       }
@@ -25,7 +26,22 @@ namespace Loops {
 
       // while-loop
       // literally the same as javascript
+      // checks to see if condition is met, if it is, codeblock is executed, this repeats
+      
 
+
+
+      // do-while loop
+      // literally the same as javascript
+      // codeblock is run first, then checks to see if condition is met, if it is, codeblock run again, if not it stops
+      int counter = 0;
+
+      do {
+        counter++;
+        System.Console.WriteLine("counter is at {0}", counter);
+      } while(counter < 6);
+
+      System.Console.WriteLine("do-while loop completed");
     }
 
   }
