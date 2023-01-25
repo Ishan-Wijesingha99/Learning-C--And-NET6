d4f428b885350521757ba4036ae71c08f13d8db9
Ishan <ishanwijes@gmail.com>
2023-01-25 23:08:49 +1100

    switch statements completed

--- 7 - Switch and Case/Program.cs
+++ 7 - Switch and Case/Program.cs
@@ -2,3 +2,31 @@
 using System;
 
 
+
+namespace SwitchStatement {
+
+  class SwithClass {
+
+    static void Main(string[] args) {
+
+      // using switch statement
+      int age = 18;
+
+      switch(age) {
+        case 15:
+          System.Console.WriteLine("Too young to enter club");
+          break;
+        case 18:
+          System.Console.WriteLine("Old enough to enter the club");
+          break;
+        default:
+          // default is executed if none of the cases match
+          System.Console.WriteLine("Does not match any of the cases");
+          break;
+      }
+
+    }
+
+  }
+
+}
