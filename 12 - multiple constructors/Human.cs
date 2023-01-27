26492a86c48289197d611bc0a2dc39f4cde84b02
Ishan <ishanwijes@gmail.com>
2023-01-27 22:31:30 +1100

    multiple constructors completed

--- 12 - multiple constructors/Human.cs
+++ 12 - multiple constructors/Human.cs
@@ -7,6 +7,13 @@ using System;
 
 namespace UsingClasses {
 
-  
+  class Cat {
+
+    // a constructor that takes in parameters is called a parameterized constructor, but one that doesn't is called a default constructor
+    public Cat() {
+      System.Console.WriteLine("Cat spawned!");
+    }
+
+  }
 
 }
