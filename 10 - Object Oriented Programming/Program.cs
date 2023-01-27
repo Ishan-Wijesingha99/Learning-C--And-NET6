d7ca3ea2b1b89395fce07fccfe8e0bdcc186425b
Ishan <ishanwijes@gmail.com>
2023-01-27 21:09:08 +1100

    add object-oriented programming files

---
+++ 10 - Object Oriented Programming/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// class - A blueprint for an object, it spits out instances (objects)
+
+
+
+using System;
+
+
+
+namespace Loops {
+
+  class LoopsClass {
+
+    static void Main(string[] args) {
+      
+    }
+    
+  }
+
+}
